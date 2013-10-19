005a9f9411cf877621a640c1181d6d7523555529
Bruno Baia <brbaia@gmail.com>
2013-10-19 19:30:12 +0200

    Fix NgBy when used with IWebElement

--- src/Protractor/JavaScriptBy.cs
+++ src/Protractor/JavaScriptBy.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 
 using OpenQA.Selenium;
+using OpenQA.Selenium.Internal;
 
 namespace Protractor
 {
@@ -32,7 +33,22 @@ namespace Protractor
             scriptArgs[0] = this.RootElement;
             Array.Copy(this.args, 0, scriptArgs, 1, this.args.Length);
 
-            ReadOnlyCollection<IWebElement> elements = ((IJavaScriptExecutor)context).ExecuteScript(this.script, scriptArgs) as ReadOnlyCollection<IWebElement>;
+            // Get JS executor
+            IJavaScriptExecutor jsExecutor = context as IJavaScriptExecutor;
+            if (jsExecutor == null)
+            {
+                IWrapsDriver wrapsDriver = context as IWrapsDriver;
+                if (wrapsDriver != null)
+                {
+                    jsExecutor = wrapsDriver.WrappedDriver as IJavaScriptExecutor;
+                }
+            }
+            if (jsExecutor == null)
+            {
+                throw new NotSupportedException("Could not get an IJavaScriptExecutor instance from the context.");
+            }
+
+            ReadOnlyCollection<IWebElement> elements = jsExecutor.ExecuteScript(this.script, scriptArgs) as ReadOnlyCollection<IWebElement>;
             if (elements == null)
             {
                 elements = new ReadOnlyCollection<IWebElement>(new List<IWebElement>(0));
