47993ec1409bb7cbbcd5943e63e04997484c3d75
Bruno Baia <brbaia@gmail.com>
2013-10-24 00:11:01 +0200

    Add sample tests using the 'ngMockE2E' module

---
+++ examples/Protractor.Samples/UsingMockModuleTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.PhantomJS;
+using OpenQA.Selenium.Chrome;
+
+using Protractor.Samples.Support;
+
+namespace Protractor.Samples
+{
+    /*
+     * E2E testing against the AngularJS tutorial Step 5 sample: 
+     * http://docs.angularjs.org/tutorial/step_05
+     */
+    [TestFixture]
+    public class UsingMockModuleTests
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void SetUp()
+        {
+            // Using NuGet Package 'PhantomJS'
+            driver = new PhantomJSDriver();
+
+            // Using NuGet Package 'WebDriver.ChromeDriver.win32'
+            //driver = new ChromeDriver();
+
+            driver.Manage().Timeouts().SetScriptTimeout(TimeSpan.FromSeconds(5));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+
+        [Test]
+        public void ShouldFilter()
+        {
+            // Fake backend with 2 phones
+            NgMockE2EModule mockModule = new NgMockE2EModule(@"
+$httpBackend.whenGET('phones/phones.json').respond(
+[
+    {
+        age: 12, 
+        carrier: 'AT&amp;T', 
+        id: 'motorola-bravo-with-motoblur', 
+        imageUrl: 'img/phones/motorola-bravo-with-motoblur.0.jpg', 
+        name: 'MOTOROLA BRAVO\u2122 with MOTOBLUR\u2122', 
+        snippet: 'An experience to cheer about.'
+    }, 
+    {
+        age: 13, 
+        carrier: 'T-Mobile', 
+        id: 'motorola-defy-with-motoblur', 
+        imageUrl: 'img/phones/motorola-defy-with-motoblur.0.jpg', 
+        name: 'Motorola DEFY\u2122 with MOTOBLUR\u2122', 
+        snippet: 'Are you ready for everything life throws your way?'
+    }, 
+]
+);
+");
+            IWebDriver ngDriver = new NgWebDriver(driver, mockModule);
+            ngDriver.Navigate().GoToUrl("http://angular.github.io/angular-phonecat/step-5/app/");
+            Assert.AreEqual(2, ngDriver.FindElements(NgBy.Repeater("phone in phones")).Count);
+            ngDriver.FindElement(NgBy.Input("query")).SendKeys("bravo");
+            Assert.AreEqual(1, ngDriver.FindElements(NgBy.Repeater("phone in phones")).Count);
+            ngDriver.FindElement(NgBy.Input("query")).SendKeys("!");
+            Assert.AreEqual(0, ngDriver.FindElements(NgBy.Repeater("phone in phones")).Count);
+        }
+    }
+}
