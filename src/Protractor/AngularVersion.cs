4f0233dd2b2a69b4e31d3a3dd37a2e2adb27f4ec
Igor-Ratsuk <igor.ratsuk@gmail.com>
2017-01-24 11:54:16 +0100

    Add ability to switch between Angular and non-Angular pages

---
+++ src/Protractor/AngularVersion.cs
@@ -0,0 +1,23 @@
+﻿namespace Protractor
+{
+    /// <summary>
+    /// Represents versions of Angular
+    /// </summary>
+    public enum AngularVersion
+    {
+        /// <summary>
+        /// No angular
+        /// </summary>
+        None,
+
+        /// <summary>
+        /// AngularJS
+        /// </summary>
+        AngularJS,
+
+        /// <summary>
+        /// Angular 2
+        /// </summary>
+        Angular2
+    }
+}
